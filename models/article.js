542938b67685fd776add8c098b7b7157adf954e7
AndreasSchaefar <skachko.andrii@gmail.com>
2022-07-10 22:41:55 +0300

    article schema

---
+++ models/article.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const articleSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 400,
+    index: true,
+  },
+  subtitle: {type: String, minlength: 5},
+  description: {type: String, required: true, minlength: 5, maxlength: 5000},
+  owner: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+  category: {
+    type: String,
+    required: true,
+    enum: ['sport', 'games', 'history'],
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: () => Date.now(),
+  },
+});
+
+module.exports = mongoose.model('Article', articleSchema);
